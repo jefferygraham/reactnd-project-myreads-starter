9862c7cff36917363f120d1e3fdbff740bc9b4bb
Jeff Graham <jgraham@dcmetrowebdevelopment.com>
2020-04-14 16:09:17 -0400

    chore: add Book component

--- src/Shelf.js
+++ src/Shelf.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Book from './Book';
 
 class Shelf extends Component {
     render() {
@@ -10,22 +11,11 @@ class Shelf extends Component {
                         <ol className="books-grid">
                             {this.props.books.filter((book) => book.shelf === this.props.shelfProp).map((book) => (
                                 <li key={book.id}>
-                                    <div className="book">
-                                        <div className="book-top">
-                                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                                            <div className="book-shelf-changer">
-                                                <select>
-                                                    <option value="move" disabled>Move to...</option>
-                                                    <option value="currentlyReading">Currently Reading</option>
-                                                    <option value="wantToRead">Want to Read</option>
-                                                    <option value="read">Read</option>
-                                                    <option value="none">None</option>
-                                                </select>
-                                            </div>
-                                        </div>
-                                        <div className="book-title">{book.title}</div>
-                                        <div className="book-authors">{book.authors.join(', ')}</div>
-                                    </div>
+                                    <Book
+                                        title={book.title}
+                                        authors={book.authors}
+                                        thumbnail={book.imageLinks.thumbnail}
+                                    />
                                 </li>
                             ))}
                         </ol>
