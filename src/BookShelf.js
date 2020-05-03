fcdd14df2c0f6725fd9726d035635c4aaa35df0e
Jeff Graham <jgraham@dcmetrowebdevelopment.com>
2020-05-02 23:04:44 -0400

    chore: add Book component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+import Book from './Book'
+
 class BookShelf extends Component {
     render() {
         console.log(this.props.books)
@@ -9,24 +11,7 @@ class BookShelf extends Component {
                 <div className="bookshelf-books">
                     <ol className="books-grid">
                         {this.props.books.filter(book => (book.shelf === this.props.shelf.id)).map(book => (
-                            <li key={book.id}>
-                                <div className="book">
-                                    <div className="book-top">
-                                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                                        <div className="book-shelf-changer">
-                                            <select>
-                                                <option value="move" disabled>Move to...</option>
-                                                <option value="currentlyReading">Currently Reading</option>
-                                                <option value="wantToRead">Want to Read</option>
-                                                <option value="read">Read</option>
-                                                <option value="none">None</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <div className="book-title">{book.title}</div>
-                                    <div className="book-authors">{book.authors.join(", ")}</div>
-                                </div>
-                            </li>
+                            <Book key={book.id} book={book} />
                         ))}
                     </ol>
                 </div>
